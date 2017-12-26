4690e4fc45484dcb20fd02a0f84153955d51c669
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2017-12-26 10:44:42 +0100

    added http post request logic to the server

--- EasyCurrency/ViewController.swift
+++ EasyCurrency/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passwTextfield: UITextField!
+    @IBOutlet weak var passLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -29,9 +31,31 @@ class ViewController: UIViewController {
     //    MARK: Actions
     
     @IBAction func SubmitBtn(_ sender: UIButton) {
-        print("utente inserito: "+nameTextField.text!)
         
-//        inserire codice per validare login
+        let url = URL(string: "http://francesco1735212.ddns.net:3000/server_app_mob/verify_user.php")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let postString = "name="+nameTextField.text!+"&password="+passwTextfield.text!
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+//                check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+        
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+//                check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+        
     }
     
     
