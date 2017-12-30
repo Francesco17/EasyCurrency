3a61b4ed44a2ea07db6a8d486e806304d41a5fda
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2017-12-30 19:16:18 +0100

    map page done with live currency exchange rates

--- EasyCurrency/MapsItem.swift
+++ EasyCurrency/MapsItem.swift
@@ -35,6 +35,7 @@ class MapsItem: UIViewController {
     @objc func handleTap(_ sender: UITapGestureRecognizer){
     
         var countryLabel = ""
+        var countryName = ""
         
         if sender.state == .ended {
             let location = sender.location(in: mapView)
@@ -53,6 +54,8 @@ class MapsItem: UIViewController {
                     if (placemarks != nil), let placemark = placemarks?.first {
                         if placemark.country != nil {
                             countryLabel = placemark.isoCountryCode!
+                            countryName = placemark.country!
+//                            print(countryName)
                         }
                         else{
                             countryLabel = "SEA"
@@ -64,8 +67,14 @@ class MapsItem: UIViewController {
                 
 //                print(countryLabel)
                 var countryCurrency = ""
+                var countryCurrencyCode = ""
+                var baseCurrency = "EUR"
+                var selCurrencyFrom = ""
+                var selCurrencyTo = ""
+                var rate = Double(1)
+                var rate_not_present = false
                 
-                func countryRequest(countryLabel: String, completion: @escaping (String)->()){
+                func countryRequest(countryLabel: String, completion: @escaping (String, String)->()){
                     let url = URL(string: "https://restcountries.eu/rest/v2/alpha/"+countryLabel)!
 //                    print(url)
                     
@@ -81,9 +90,9 @@ class MapsItem: UIViewController {
                                     
                                     if let currencies = dictResponse["currencies"] as? [[String:Any]]{
                                         
-                                        if currencies.first!["name"] != nil{
+                                        if currencies.first!["name"] != nil {
                                             countryCurrency = currencies.first!["name"]! as! String
-                                            
+                                            countryCurrencyCode = currencies.first!["code"] as! String
                                         }
                                         else{
                                             print("Currency not present")
@@ -96,25 +105,81 @@ class MapsItem: UIViewController {
                             }
                             
                         }
-                        completion(countryCurrency)
+                        completion(countryCurrency, countryCurrencyCode)
                     }
                     task.resume()
                     
                 }
                 
-                countryRequest(countryLabel: countryLabel){ countryCurrency in
+                countryRequest(countryLabel: countryLabel){ (countryCurrency, countryCurrencyCode) in
+                    
+                    func getRates(selCurrencyFrom: String, selCurrencyTo: String, completion: @escaping (Double)->()){
 
-                    DispatchQueue.main.async {
-                        self.isTappingNow = true
-                        self.mapView.removeAnnotations(self.mapView.annotations)
-                        let locat:CLLocationCoordinate2D = CLLocationCoordinate2DMake(locationCoord.latitude, locationCoord.longitude)
-                        let annotation = MKPointAnnotation()
-                        annotation.coordinate = locat
-                        annotation.title = countryCurrency
-                        annotation.subtitle = "subtitle"
-                        self.mapView.addAnnotation(annotation)
-                        self.mapView.selectAnnotation(annotation, animated: true)
-                        self.isTappingNow = false
+                        let url = URL(string: "https://api.fixer.io/latest?base="+selCurrencyFrom+"&symbols="+selCurrencyTo)
+                        
+                        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                            
+                            if error != nil {
+                                print("HTTP request error")
+                            }
+                            else{
+                                do{
+                                    let json = try JSONSerialization.jsonObject(with: data!)
+                                    if let dictResponse = json as? [String:Any] {
+                                        
+                                        if let currencies = dictResponse["rates"] as? [String:Any]{
+                                            
+                                            if currencies[selCurrencyTo] != nil {
+                                                rate = (currencies[selCurrencyTo] as? Double)!
+                                                //print(rate)
+                                                
+                                            }
+                                            else{
+                                                if selCurrencyTo == baseCurrency {
+                                                    rate = 1.0
+                                                }
+                                                else {
+                                                    print("Rate not present")
+                                                    rate_not_present = true
+                                                }
+                                            }
+                                        }
+                                    }
+                                }catch {
+                                    print("Error parsing Json")
+                                }
+                            }
+                            completion(rate)
+                        }
+                        task.resume()
+                    }
+                    
+                    getRates(selCurrencyFrom: baseCurrency, selCurrencyTo: countryCurrencyCode){ rate in
+                        
+                        DispatchQueue.main.async {
+                            self.isTappingNow = true
+                            self.mapView.removeAnnotations(self.mapView.annotations)
+                            let locat:CLLocationCoordinate2D = CLLocationCoordinate2DMake(locationCoord.latitude, locationCoord.longitude)
+                            let annotation = MKPointAnnotation()
+                            annotation.coordinate = locat
+                            if (countryName != "" || countryCurrency != "") {
+                                if rate_not_present {
+                                    annotation.title = countryName+", ("+countryCurrency+")"
+                                    annotation.subtitle = "Exchange not available, try another country.."
+                                }
+                                else{
+                                    annotation.title = countryName+", ("+countryCurrency+")"
+                                    annotation.subtitle = "1 "+baseCurrency+" = "+String(rate)+" "+countryCurrencyCode
+                                }
+                            }
+                            else{
+                                annotation.title = "You tap the sea!"
+                            }
+                            self.mapView.addAnnotation(annotation)
+                            self.mapView.selectAnnotation(annotation, animated: true)
+                            self.isTappingNow = false
+                            
+                        }
                         
                     }
                     
