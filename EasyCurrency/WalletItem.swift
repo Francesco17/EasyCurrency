2ed6a2cded7acfcc067c619293a26b7e8c016d16
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2017-12-31 18:23:40 +0100

    improved layout of wallet page

--- EasyCurrency/WalletItem.swift
+++ EasyCurrency/WalletItem.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class WalletItem: UIViewController {
-
+    
+//    MARK: properties
+    
+    @IBOutlet weak var usernameTextField: UILabel!
+    @IBOutlet weak var depositTextField: UILabel!
+    @IBOutlet weak var balanceTextField: UILabel!
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,8 +25,12 @@ class WalletItem: UIViewController {
 //        getting stored informations
         let defaults = UserDefaults.standard
         if let userName_logged = defaults.string(forKey: "username"){
-            print(userName_logged)
+            self.usernameTextField.text = "Username: "+userName_logged.capitalized
         }
+        self.depositTextField.text = "Deposit = 100..to be done"
+        self.balanceTextField.text = "Balance = 100..to be done"
+        
+        tableview.tableFooterView = UIView(frame: CGRect.zero)
         
     }
 
@@ -27,6 +39,9 @@ class WalletItem: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func addTransBtn(_ sender: UIBarButtonItem) {
+        
+    }
     
     @IBAction func logoutBtn(_ sender: UIBarButtonItem) {
         OperationQueue.main.addOperation {
