1d039f04e2b27ebd5e2326aac790613cfb3bd7fc
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-01-01 20:50:13 +0100

    started logic for wallet page, done mostly the backend

--- EasyCurrency/WalletItem.swift
+++ EasyCurrency/WalletItem.swift
@@ -16,7 +16,9 @@ class WalletItem: UIViewController {
     @IBOutlet weak var depositTextField: UILabel!
     @IBOutlet weak var balanceTextField: UILabel!
     
-    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+//    var transactions = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,7 @@ class WalletItem: UIViewController {
         self.depositTextField.text = "Deposit = 100..to be done"
         self.balanceTextField.text = "Balance = 100..to be done"
         
-        tableview.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
         
     }
 
@@ -49,3 +51,26 @@ class WalletItem: UIViewController {
         }
     }
 }
+
+//extension WalletItem: UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return transactions.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+//        cell.textLabel?.text = ""
+//        cell.detailTextLabel?.text = ""
+//        return cell
+//    }
+//}
+
+
+
+
+
+
