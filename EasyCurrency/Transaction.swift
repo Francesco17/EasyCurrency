44f88cb6bd721f9721b63f69b4058507370403fb
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-01-02 17:38:25 +0100

    saveTrans() method solved

--- EasyCurrency/Transaction.swift
+++ EasyCurrency/Transaction.swift
@@ -13,10 +13,10 @@ class Transaction {
     var amount: Double
     var currency: String
     var rate: Double
-    var user: String
+    var user: Int
     var id: String
     
-    init(amount: Double, currency: String, rate: Double, user: String) {
+    init(amount: Double, currency: String, rate: Double, user: Int) {
         self.amount = amount
         self.currency = currency
         self.rate = rate
@@ -30,31 +30,37 @@ class Transaction {
         var request = URLRequest(url: url)
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        let postString = "amount="+String(self.amount)+"&currency="+self.currency+"&rate="+String(self.rate)+"&user_id="+self.user
+        let postString = "amount="+String(self.amount)+"&currency="+self.currency+"&rate="+String(self.rate)+"&user_id="+String(self.user)
         request.httpBody = postString.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print("HTTP request error")
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                //                check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
             }
-            else{
-                let responseString = String(data: data!, encoding: .utf8)
-                let responseString2 = responseString?.data(using:.utf8)!
-                do{
-                    let json = try JSONSerialization.jsonObject(with: responseString2!)
-                    if let dictResponse = json as? [String:Any] {
-                        if let state = dictResponse["state"] as? String{
-                            if state == "SUCCESS" {
-                                self.id = dictResponse["id"] as! String
-                                print("Transazione salvata")
-                            }
-                            else{
-                                print("Error occurred")
-                            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                //                check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            let responseString = String(data: data, encoding: .utf8)
+            let responseString2 = responseString?.data(using:.utf8)!
+            print("responseString = \(String(describing: responseString))")
+            do{
+                let json = try JSONSerialization.jsonObject(with: responseString2!)
+                if let dictionary = json as? [String: Any]{
+                    if let state = dictionary["state"] as? String{
+                        if state == "SUCCESS" {
+                            self.id = dictionary["id"] as! String
+                        }
+                        else if state == "FAIL" {
+                            print("Error occurred")
                         }
                     }
-                }catch {
-                    print("Error parsing Json")
                 }
+            }
+            catch {
+                print("Error parsing Json")
             }
         }
         task.resume()
