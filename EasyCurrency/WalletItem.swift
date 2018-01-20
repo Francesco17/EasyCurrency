36d1e1f25b4393b75d1cbde952c0ca72cf82e7e8
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-01-20 16:23:33 +0100

    logic for updating balance on viewDidLoad done. Still balance display on label to fix.

--- EasyCurrency/WalletItem.swift
+++ EasyCurrency/WalletItem.swift
@@ -41,54 +41,92 @@ class WalletItem: UIViewController {
             self.user_id = Int(user_id)!
         }
         
-        get_depbal(id: self.user_id) { (dep, bal) in
-            self.defaults.set(dep, forKey: "deposit")
-            DispatchQueue.main.async {
-                self.depositTextField.text = "Deposit = "+dep
-                self.balanceTextField.text = "Balance = "+bal
-            }
-        }
-        
-        func get_trans(user_id: Int, completion: @escaping ()->()){
-    
-            let url = URL(string: "http://francesco1735212.ddns.net:3000/server_app_mob/get_transactions.php?user_id="+String(user_id) )
-            let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-                
-                if error != nil {
-                    print("HTTP request error")
-                }
-                else{
-                    do{
-                        let json = try JSONSerialization.jsonObject(with: data!) as? [String: Any]
-                        if let trans = json!["transactions"] as? [[String:Any]]{
-                            for tra in trans{
-                                let id = String(tra["id"] as! String)
-                                let am = Double(tra["amount"] as! String)
-                                let cu = String(tra["currency"] as! String)
-                                let ra = Double(tra["rate"] as! String)
-                                let tr = Transaction(id: id, amount: am!, currency: cu, rate: ra!, user: user_id)
-                                transactions.append(tr)
-                            }
-                        }
-                        else{
-                            print("Error")
-                        }
-                    }catch {
-                        print("Error parsing Json")
-                    }
-                }
-                completion()
-            }
-            task.resume()
-        }
-        
         get_trans(user_id: self.user_id) {
             OperationQueue.main.addOperation {
                 self.tableView.reloadData()
+                self.computeNewBalance(trans: transactions)
+            }
+            
+            self.get_depbal(id: self.user_id) { (dep, bal) in
+                self.defaults.set(dep, forKey: "deposit")
+                DispatchQueue.main.async {
+                    self.depositTextField.text = "Deposit = "+dep
+                    self.balanceTextField.text = "Balance = "+String(self.balance)
+                }
             }
         }
         
         tableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
+    
+    func computeNewBalance(trans: [Transaction]){
+        
+        self.balance = defaults.double(forKey: "deposit")
+        
+        var buff = String()
+        for transaction in trans {
+            if buff.isEmpty {
+                buff = transaction.currency
+            }
+            else{
+                buff = buff+","+transaction.currency
+            }
+        }
+        
+        getRates(selCurrencyFrom: "EUR", selCurrencyTo: buff) { (rates) in
+
+            for i in 0...trans.count-1{
+                let oldAmount = trans[i].amount*trans[i].rate
+                print("oldAmount: "+String(oldAmount))
+                let newAmount = oldAmount/rates[i]
+                print("newAmount: "+String(newAmount))
+                self.balance += newAmount
+            }
+            print("balance: "+String(self.balance))
+            self.updateBalance(bal: self.balance)
+        }
+
+    }
+    
+    func updateBalance(bal: Double){
+        //        update deposit
+        let url2 = URL(string: "http://francesco1735212.ddns.net:3000/server_app_mob/update_balance_deposit.php")!
+        var request2 = URLRequest(url: url2)
+        request2.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request2.httpMethod = "POST"
+        let postString2 = "deposit=&balance="+String(bal)+"&user_id="+String(self.user_id)
+        request2.httpBody = postString2.data(using: .utf8)
+        let task2 = URLSession.shared.dataTask(with: request2) { data2, response2, error2 in
+            guard let dat = data2, error2 == nil else {
+//              check for fundamental networking error
+                print("error=\(String(describing: error2))")
+                return
+            }
+            if let httpStatus = response2 as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                //                check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response2))")
+            }
+            let responseStr = String(data: dat, encoding: .utf8)
+            let responseStr2 = responseStr?.data(using:.utf8)!
+            do{
+                let json = try JSONSerialization.jsonObject(with: responseStr2!)
+                if let dictionary = json as? [String: Any]{
+                    if let state = dictionary["state"] as? String{
+                        if state == "SUCCESS" {
+                            print("balance updated")
+                        }
+                        else if state == "FAIL" {
+                            print("Error occurred")
+                        }
+                    }
+                }
+            }
+            catch {
+                print("Error parsing Json")
+            }
+        }
+        task2.resume()
     }
     
     func get_depbal(id: Int, completion: @escaping (String, String)->()){
@@ -124,8 +162,42 @@ class WalletItem: UIViewController {
         task.resume()
     }
     
+    func get_trans(user_id: Int, completion: @escaping ()->()){
+        
+        let url = URL(string: "http://francesco1735212.ddns.net:3000/server_app_mob/get_transactions.php?user_id="+String(user_id) )
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                print("HTTP request error")
+            }
+            else{
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data!) as? [String: Any]
+                    if let trans = json!["transactions"] as? [[String:Any]]{
+                        for tra in trans{
+                            let id = String(tra["id"] as! String)
+                            let am = Double(tra["amount"] as! String)
+                            let cu = String(tra["currency"] as! String)
+                            let ra = Double(tra["rate"] as! String)
+                            let tr = Transaction(id: id, amount: am!, currency: cu, rate: ra!, user: user_id)
+                            transactions.append(tr)
+                        }
+                    }
+                    else{
+                        print("Error")
+                    }
+                }catch {
+                    print("Error parsing Json")
+                }
+            }
+            completion()
+        }
+        task.resume()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.tableView.reloadData()
+//        self.computeNewBalance(trans: transactions)
     }
 
     override func didReceiveMemoryWarning() {
@@ -173,8 +245,8 @@ extension WalletItem: UITableViewDelegate, UITableViewDataSource {
             let foreignAmount = transactions[indexPath.row].amount * transactions[indexPath.row].rate;
             let baseCurrency = "EUR"
             
-            getRates(selCurrencyFrom: transactions[indexPath.row].currency, selCurrencyTo: baseCurrency, completion: { (rat) in
-                let amountBack = foreignAmount*rat
+            getRates(selCurrencyFrom: transactions[indexPath.row].currency, selCurrencyTo: baseCurrency, completion: { (rates) in
+                let amountBack = foreignAmount*rates[0]
                 let oldDeposit = self.defaults.double(forKey: "deposit")
                 let newDeposit = oldDeposit + amountBack
                 transactions[indexPath.row].updateDep(dep: newDeposit)
@@ -194,8 +266,8 @@ extension WalletItem: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
-    func getRates(selCurrencyFrom: String, selCurrencyTo: String, completion: @escaping (Double)->()){
-        var rat = Double(1)
+    func getRates(selCurrencyFrom: String, selCurrencyTo: String, completion: @escaping ([Double])->()){
+        var rates = [Double]()
         let url = URL(string: "https://api.fixer.io/latest?base="+selCurrencyFrom+"&symbols="+selCurrencyTo)
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
             if error != nil {
@@ -204,13 +276,23 @@ extension WalletItem: UITableViewDelegate, UITableViewDataSource {
             else{
                 do{
                     let json = try JSONSerialization.jsonObject(with: data!)
+                    let currArray = selCurrencyTo.split(separator: ",")
                     if let dictResponse = json as? [String:Any] {
                         if let currencies = dictResponse["rates"] as? [String:Any]{
-                            if currencies[selCurrencyTo] != nil {
-                                rat = (currencies[selCurrencyTo] as? Double)!
+                            if currArray.count > 1{
+                                for i in 0...currArray.count-1{
+//                                    print(currencies["USD"])
+                                    rates.append(currencies[String(currArray[i])] as! Double)
+                                }
                             }
                             else{
-                                print("Rate not present")
+                                if currencies[selCurrencyTo] != nil {
+                                    rates.append((currencies[selCurrencyTo] as? Double)!)
+                                }
+                                else{
+                                    print("Rate not present")
+                                    rates.append(1)
+                                }
                             }
                         }
                     }
@@ -218,7 +300,7 @@ extension WalletItem: UITableViewDelegate, UITableViewDataSource {
                     print("Error parsing Json")
                 }
             }
-            completion(rat)
+            completion(rates)
         }
         task.resume()
     }
