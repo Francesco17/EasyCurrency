f3250913e250cb249a6472fae3cc48688c06312e
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-01-21 17:53:27 +0100

    touch-id identification commented for the moment

--- EasyCurrency/ViewController.swift
+++ EasyCurrency/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        touchIdAuthentication()
+//        touchIdAuthentication()
     }
 
     override func didReceiveMemoryWarning() {
