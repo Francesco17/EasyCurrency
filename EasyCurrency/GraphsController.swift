4dc66b32169899152ba59598a97fa1ce8fa426b6
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-02-17 18:58:38 +0100

    computed dates on x axis and written almost all of the function that gets the rates

--- EasyCurrency/GraphsController.swift
+++ EasyCurrency/GraphsController.swift
@@ -17,9 +17,32 @@ class GraphsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var today = Date()
+        var dateArray = [String]()
+        var dateArray_2 = [String]()
+        for _ in 1...16{
+            let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today)
+            let formatter = DateFormatter()
+            let formatter_2 = DateFormatter()
+            formatter.dateFormat = "dd"
+            formatter_2.dateFormat = "yyyy-MM-dd"
+            let stringDate: String = formatter.string(from: today)
+            let strigDate_2: String = formatter_2.string(from: today)
+            today = yesterday!
+            dateArray.append(stringDate)
+            dateArray_2.append(strigDate_2)
+        }
+        dateArray.remove(at: 0)
+        dateArray_2.remove(at: 0)
+        
+        getRates(selCurrencyTo: title_graph, dateArray: dateArray_2) { (rates) in
+            print(rates)
+        }
+        
         var views: [String: AnyObject] = [:]
         
-        label.text = title_graph
+        label.text = "History conversion from EUR to "+title_graph
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = NSTextAlignment.center
         self.view.addSubview(label)
@@ -28,11 +51,11 @@ class GraphsController: UIViewController {
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-120-[label]", options: [], metrics: nil, views: views))
         
         // simple arrays
-        let data: [CGFloat] = [3, 4, -2, 11, 13, 15]
+        let data: [CGFloat] = [3, 4, -2, 11, 13, 15,7,7,7,7,7,6,6,6,6]
 //        let data2: [CGFloat] = [1, 3, 5, 13, 17, 20]
         
         // simple line with custom x axis labels
-        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+//        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
         
         lineChart = LineChart()
         lineChart.animation.enabled = true
@@ -40,7 +63,7 @@ class GraphsController: UIViewController {
         lineChart.x.labels.visible = true
         lineChart.x.grid.count = 10
         lineChart.y.grid.count = 10
-        lineChart.x.labels.values = xLabels
+        lineChart.x.labels.values = dateArray
         lineChart.y.labels.visible = true
         lineChart.addLine(data)
 //        lineChart.addLine(data2)
@@ -51,6 +74,43 @@ class GraphsController: UIViewController {
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[chart]-|", options: [], metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label]-[chart]-|", options: [], metrics: nil, views: views))
 //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label]-[chart(==200)]", options: [], metrics: nil, views: views))
+    }
+
+    
+    func getRates(selCurrencyTo: String, dateArray: [String], completion: @escaping ([Double])->()){
+        var rates = [Double]()
+        for i in 1...dateArray.count-1{
+            let url = URL(string: "https://api.fixer.io/"+dateArray[i]+"?symbols="+selCurrencyTo)
+            print(url!)
+            let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                if error != nil {
+                    print("HTTP request error")
+                }
+                else{
+                    do{
+                        let json = try JSONSerialization.jsonObject(with: data!)
+                        if let dictResponse = json as? [String:Any] {
+                            if let currs = dictResponse["rates"] as? [String:Any]{
+                                if currs[selCurrencyTo] != nil {
+                                    rates.append(currs[selCurrencyTo] as! Double)
+                                }
+                                else{
+                                    rates.append(-1)
+                                }
+                            }
+                            else{
+                                print("Rate not present")
+                                rates.append(-2)
+                            }
+                        }
+                    }catch {
+                        print("Error parsing Json")
+                    }
+                }
+            }
+            task.resume()
+        }
+        
     }
     
 
